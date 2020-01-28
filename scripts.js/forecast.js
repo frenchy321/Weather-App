47d28cb197593a47ae15bddf3980964ed1be12ad
Andy <andy.french@hotmail.co.uk>
2020-01-28 17:33:01 +0000

    Created getCity function

--- scripts.js/forecast.js
+++ scripts.js/forecast.js
@@ -1,1 +1,10 @@
 const key = 'kAXQrAvFqJCQhb7ifzlN1ADrlrd0B4Wn';
+const getCity = async city => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  console.log(data);
+};
+
+getCity('swindon');
